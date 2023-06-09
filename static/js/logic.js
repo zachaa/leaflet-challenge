b63db3295eafa19fed22a11a76740decb1b89efd
zachaa <57045291+zachaa@users.noreply.github.com>
2023-06-09 14:02:45 -0500

    add map code, legend CSS + add chroma.js package

---
+++ static/js/logic.js
@@ -0,0 +1,147 @@
+const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+let gradientColors = ["#B6FFC2", "#59C0ED", "#0065B2", "#761DAD", "#CD3300"];
+let allDepths;
+let colorDomain;
+let chromaScale;
+
+d3.json(url).then(data => {
+    allDepths = getDepths(data.features);
+    // console.log(Math.max(...allDepths));
+    // console.log(chroma.limits(allDepths, 'l', 4));
+    colorDomain = chroma.limits(allDepths, 'l', gradientColors.length - 1);
+    chromaScale = chroma.scale(gradientColors).domain(colorDomain);
+    createEarthquakeFeatures(data.features);
+});
+
+
+function getDepths(features) {
+    let depths = []
+    features.forEach(element => {
+        let value = element.geometry.coordinates[2];
+        depths.push(value > 0 ? value : 0.001);
+    });
+    return depths;
+}
+
+
+function colorDepth(depth) {
+    return chromaScale(depth).hex();
+}
+
+function radiusMagnitude(magnitude) {
+    return magnitude * 4;
+}
+
+function _addPopup(feature, layer) {
+    layer.bindPopup(`${feature.properties.place}
+                    <hr>
+                    Depth: ${feature.geometry.coordinates[2]} km<br>
+                    Magnitude: ${feature.properties.mag}<br>
+                    ${new Date(feature.properties.time).toISOString()} UTC`)
+}
+
+function _pointToLayer(feature, latLong) {
+    return L.circleMarker(latLong, {
+        color: colorDepth(feature.geometry.coordinates[2]),
+        fillColor: colorDepth(feature.geometry.coordinates[2]),
+        fillOpacity: 0.75,
+        stroke: false,
+        radius: radiusMagnitude(feature.properties.mag),
+    })
+}
+
+
+function createEarthquakeFeatures(earthquakeData) {
+    let earthquakes = L.geoJSON(earthquakeData,
+        {onEachFeature: _addPopup,
+         pointToLayer: _pointToLayer,
+        });
+    
+    createMap(earthquakes);
+}
+
+
+function createLegend() {
+    let legend = L.control({position: "bottomright"});
+
+    legend.onAdd = () => {
+        let div = L.DomUtil.create("div", "legend");
+
+        // create list of html <li> elements with scale values
+        let htmlLI = []
+        colorDomain.forEach((value, index) => {
+            // smallest value is slightly greater than 0, so we will display it as 0.00 +
+            let formattedValue = index == 0 ? "0.00 +" : value.toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 });
+            htmlLI.push("<li>" + formattedValue + "</li>");
+        });
+
+        // create list of css hex colors
+        // let cssGradient = [];
+        // chromaScale.forEach(color => {
+        //     cssGradient.push(color.hex());
+        // });
+        // let cssGradient = gradientColors.join(", ");
+
+        console.log(htmlLI);
+        // console.log(cssGradient);
+        div.innerHTML = `<h3 class="legend_title">Depth (km)</h3>
+                        <div class="data_container">
+                            <div class="gradient" style="background: linear-gradient(${gradientColors.join(", ")})"></div>
+                            <div class="gradient_values">
+                                <ul>
+                                    ${htmlLI.join('')}
+                                </ul>
+                            </div>
+                        </div>`;
+        return div;
+    }
+    return legend;
+}
+
+
+function createMap(earthquakes) {
+    // For tile map options see: https://leaflet-extras.github.io/leaflet-providers/preview/
+    let esriGray = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+        maxZoom: 16
+    });
+
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+
+    let USGS_USImagery = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {
+        maxZoom: 20,
+        attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+    });
+
+    let baseMaps = {
+        Base: esriGray,
+        Streets: street,
+        Satellite: USGS_USImagery
+    };
+
+    let overlayMaps = {
+        Earthquakes: earthquakes
+    };
+
+    // create the map
+    let earthquakeMap = L.map("map", {
+        center: [37.0902, -95.7129],
+        zoom: 4,
+        layers: [esriGray, earthquakes]
+    });
+
+    // add the layers
+    L.control.layers(
+        baseMaps,
+        overlayMaps,
+        {collapsed: false})
+        .addTo(earthquakeMap);
+    
+    // add the legend
+    let legend = createLegend();
+    legend.addTo(earthquakeMap);
+}
